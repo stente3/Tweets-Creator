5d93195d6492a3672520e32a701628ba233557da
stente3 <stentem0103@gmail.com>
2021-09-21 17:02:01 -0500

    Now tweets can be viewed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,40 @@
- 
+//Variables
+const form = document.querySelector(".form");
+const formTweets = document.querySelector("#tweet");
+const formButton = document.querySelector(".form__button");
+const tweetsList = document.querySelector(".tweets__list");
+const ErrorAlert = document.querySelector(".alert");
+let tweets = [];
+
+//Event Listeners
+formButton.addEventListener("click", validator);
+
+
+//Funtions
+function validator() {
+    if(formTweets.value === ""){
+        ErrorAlert.classList.remove("d-none");
+        setTimeout(() => {
+            ErrorAlert.classList.add("d-none");
+        }, 5000);
+    } else{
+        getTweets();
+    }
+}
+function getTweets() {
+    let tweetObj = {
+        value: formTweets.value,
+        id: Date.now()
+    }
+    tweets = [...tweets, tweetObj];
+    form.reset();
+    formTweets.focus();
+    addHtml(tweetObj);
+}
+function addHtml(tweetObj) {
+    let newTweet = document.createElement("li");
+    newTweet.classList.add("new__item");
+    newTweet.setAttribute("id", tweetObj.id);
+    newTweet.innerHTML =  `${tweetObj.value}<span class="item__img"></span>`;
+    tweetsList.appendChild(newTweet);
+}
